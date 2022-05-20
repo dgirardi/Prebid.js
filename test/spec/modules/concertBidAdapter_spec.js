e5c71ddf2f107694c39464668458d996a563f8a5
Demetrio Girardi <dgirardi@prebid.org>
2022-05-20 08:59:02 -0700

    convert bidders: concert

--- test/spec/modules/concertBidAdapter_spec.js
+++ test/spec/modules/concertBidAdapter_spec.js
@@ -25,7 +25,7 @@ describe('ConcertAdapter', function () {
 
     bidRequest = {
       refererInfo: {
-        referer: 'https://www.google.com'
+        page: 'https://www.google.com'
       },
       uspConsent: '1YYY',
       gdprConsent: {}
