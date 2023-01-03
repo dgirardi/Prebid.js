c1b5bbdf72eff220d2fbb5be802264d3542d5e64
Demetrio Girardi <dgirardi@prebid.org>
2023-01-03 10:15:29 -0700

    Scattered bid adapter: patch to use ortbConverter

--- modules/scatteredBidAdapter.js
+++ modules/scatteredBidAdapter.js
@@ -1,12 +1,10 @@
 // jshint esversion: 6, es3: false, node: true
 'use strict';
 
-import { registerBidder } from '../src/adapters/bidderFactory.js';
-import { config } from '../src/config.js';
-import { BANNER } from '../src/mediaTypes.js';
-import { mergeDeep, deepAccess, parseSizesInput, parseUrl, deepSetValue, logInfo, createTrackPixelHtml } from '../src/utils.js';
-
-const { getConfig } = config;
+import {registerBidder} from '../src/adapters/bidderFactory.js';
+import {BANNER} from '../src/mediaTypes.js';
+import {deepAccess, logInfo, parseSizesInput} from '../src/utils.js';
+import {ortbConverter} from '../libraries/ortbConverter/converter.js';
 
 const BIDDER_CODE = 'scattered';
 
@@ -33,6 +31,17 @@ export function buildImps(validBidRequests) {
   });
 }
 
+export const converter = ortbConverter({
+  context: {
+    mediaType: BANNER,
+    ttl: 360
+  },
+  bidResponse(buildBidResponse, bid, context) {
+    context.netRevenue = context.bidRequest.netRevenue === 'net';
+    return buildBidResponse(bid, context);
+  }
+})
+
 export const spec = {
   code: BIDDER_CODE,
   supportedMediaTypes: [BANNER],
@@ -52,108 +61,21 @@ export const spec = {
     return true
   },
 
-  // 2.
-  buildRequests: function (validBidRequests, bidderRequest) {
-    const ortb2Obj = bidderRequest.ortb2 || {};
-    let { user } = ortb2Obj
-
-    let site = getConfig('site') || {};
-    if (ortb2Obj.site) {
-      mergeDeep(site, ortb2Obj.site)
-    }
-    // site.page    URL of the page where the impression will be shown.
-    // site.domain  Domain of the site (e.g., “mysite.foo.com”).
-    if (!site.page && bidderRequest.refererInfo) {
-      site.page = bidderRequest.refererInfo.referer;
-      site.domain = parseUrl(bidderRequest.refererInfo.referer).hostname
-    }
-
-    const bidderDomain = getKeyOnAny(validBidRequests, 'params.bidderDomain')
-
-    const device = getConfig('device') || {};
-    device.w = device.w || window.innerWidth;
-    device.h = device.h || window.innerHeight;
-    device.ua = device.ua || navigator.userAgent;
-
-    const imps = buildImps(validBidRequests)
-
-    const tid = getKeyOnAny(validBidRequests, 'transactionId');
-
-    const request = {
-      id: bidderRequest.auctionId,
-      imp: imps,
-      site,
-      user,
-      device,
-      source: { tid }
-    };
-
-    if (bidderRequest && bidderRequest.gdprConsent) {
-      let { gdprApplies, consentString } = bidderRequest.gdprConsent;
-      deepSetValue(request, 'user.ext.consent', consentString);
-      deepSetValue(request, 'regs.ext.gdpr', gdprApplies ? 1 : 0);
-    }
-
-    const test = getKeyOnAny(validBidRequests, 'params.test') || 0
-    if (test) {
-      request.test = 1;
-    }
-
+  buildRequests: function (bidRequests, bidderRequest) {
     return {
       method: 'POST',
-      url: 'https://' + bidderDomain,
-      data: JSON.stringify(request),
+      url: 'https://' + getKeyOnAny(bidderRequest, 'params.bidderDomain'),
+      data: converter.toORTB({bidderRequest, bidRequests}),
       options: {
         contentType: 'application/json'
       },
-      bidRequests: validBidRequests
     };
   },
 
   // 3.
-  interpretResponse: function (serverResponse, request) {
-    if (!serverResponse.body) {
-      return;
-    }
-
-    const { bidRequests } = request;
-    const { seatbid, cur } = serverResponse.body;
-    let idx = 0;
-    let results = []
-
-    if (seatbid !== undefined) {
-      for (let seat of seatbid) {
-        for (let bidResponse of seat.bid) {
-          const bidRequest = bidRequests[idx]
-
-          const result = {
-            requestId: bidRequest.bidId,
-            cpm: bidResponse.price,
-            creativeId: bidResponse.crid,
-            ttl: 360,
-            netRevenue: bidRequest.netRevenue === 'net',
-            currency: cur,
-            ad: bidResponse.adm,
-            mediaType: BANNER, // we don't serve other types
-            width: bidResponse.w,
-            height: bidResponse.h,
-          }
-
-          if (bidResponse.dealid) {
-            result.dealId = bidResponse.dealid
-          }
-
-          // insert pixel into markup that will call us
-          if (bidResponse.nurl) {
-            result.ad += createTrackPixelHtml(bidResponse.nurl)
-          }
-
-          results.push(result);
-          idx++;
-        }
-      }
-    }
-    return results;
+  interpretResponse: function (response, request) {
+    if (!response.body) return;
+    return converter.fromORTB({response: response.body, request: request.data}).bids;
   },
 
   // 4
