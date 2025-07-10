c971eaa32942d79f1e1a4363c5d4a983bdd40f88
Demetrio Girardi <demetrio.girardi@proton.me>
2025-07-10 13:48:07 -0700

    Update ajax.ts

--- src/ajax.ts
+++ src/ajax.ts
@@ -6,7 +6,7 @@ import { hook } from './hook.js';
 import {buildUrl, hasDeviceAccess, logError, parseUrl} from './utils.js';
 
 export const uncovered = (a, b) => {
-  return a + b; // without comment 
+  return a + b; // with comment 
 }
 
 export const dep = {
